8f7e9b04e67f88e9a5ddee5abec903be5237332f
H1ddenGames <skarim3000@gmail.com>
2018-11-20 20:54:25 -0800

    LAMP Server serving webpages properly
    Updated the lampremote.sh script to work in an almost non-interactive environment.
    Updated the configuration folder with new laravel.conf file that works properly.
    
    To do:
    - Update the lampremote.sh to be work fully non-interactive.
    - Need to create a database called irrigation.
    - Need to copy the .env file from packaged-automation to the project directory.
    - Need to cd into the project directory and run php artisan migrate
    
    
    Signed-off-by: H1ddenGames <skarim3000@gmail.com>

--- Project 7/Automation/Bash Files/lampremote.sh
+++ Project 7/Automation/Bash Files/lampremote.sh
@@ -5,7 +5,7 @@
 sudo apt-get -y update && \
 sudo add-apt-repository -y ppa:ondrej/php && \
 sudo apt-get update && \
-sudo apt-get -y install net-tools curl apache2 mysql-server libapache2-mod-php7.2 php7.2 php7.2-xml php7.2-gd php7.2-opcache php7.2-mbstring phpmyadmin php-mbstring php-gettext php-mysql
+sudo apt-get -y install dos2unix git net-tools curl apache2 mysql-server libapache2-mod-php7.2 php7.2 php7.2-xml php7.2-gd php7.2-opcache php7.2-mbstring phpmyadmin php-mbstring php-gettext php-mysql
 # ---------------------------------------
 # Installing required software finished.
 # ---------------------------------------
@@ -13,7 +13,7 @@ sudo apt-get -y install net-tools curl apache2 mysql-server libapache2-mod-php7.
 # Configuring mysql server starting.
 # ---------------------------------------
 sudo mysql -e "UPDATE mysql.user SET authentication_string = PASSWORD('password') WHERE User = 'root'"
-sudo mysql -e "DROP USER ''@'localhost'"
+sudo mysql -e "DROP USER ''@'ubuntu'"
 sudo mysql -e "DROP USER ''@'$(hostname)'"
 sudo mysql -e "DROP DATABASE test"
 sudo mysql -e "FLUSH PRIVILEGES"
@@ -28,8 +28,8 @@ yes | sudo cp phpmyadmin.conf /etc/apache2/conf-available/
 yes | sudo cp .htacess /usr/share/phpmyadmin/
 sudo phpenmod mbstring
 sudo systemctl restart apache2
-sudo mysql -e "CREATE USER 'software-irrigation'@'localhost' IDENTIFIED BY 'password';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'software-irrigation'@'localhost' WITH GRANT OPTION;"
+sudo mysql -e "CREATE USER 'software-irrigation'@'ubuntu' IDENTIFIED BY 'password';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'software-irrigation'@'ubuntu' WITH GRANT OPTION;"
 echo "password" | sudo htpasswd -i -c /etc/phpmyadmin/.htpasswd software-irrigation
 # ---------------------------------------
 # Configuring php priority and phpmyadmin finished.
@@ -41,6 +41,9 @@ cd /tmp && curl -sS https://getcomposer.org/installer | php && \
 sudo mv composer.phar /usr/local/bin/composer
 cd /var/www/html && \
 sudo composer create-project laravel/laravel software-irrigation --prefer-dist
+cd /home/software-irrigation/Desktop && git clone https://github.com/CSUN-Irrigation/software-irrigation
+sudo cp -R /home/software-irrigation/Desktop/software-irrigation /var/www/html/software-irrigation
+sudo cp laravel.conf /etc/apache2/sites-available/
 sudo chgrp -R www-data /var/www/html/software-irrigation
 sudo chmod -R 775 /var/www/html/software-irrigation/storage
 # ---------------------------------------
